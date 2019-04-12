c0ee14d6e08ae6803c3bb3ca72ae5c8e16f400f3
Zak Dehlawi <zdehlawi@gmail.com>
2019-04-12 00:02:48 -0400

    So bad at git

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ MAINTAINER Zaxim <zaxim@me.com>
 # C.UTF-8 needed to make ocrmypdf work
 ENV SCANNER_NAME="DCP-L2540DW" SCANNER_MODEL="DCP-L2540DW" SCANNER_IP_ADDRESS="192.168.2.13" LC_ALL="C.UTF-8" LANG="C.UTF-8"
 
-RUN apt-get -y update && apt-get -y upgrade && apt-get install -y python3-pip sane sane-utils libusb-0.1 ghostscript netpbm ocrmypdf && apt-get -y clean && rm -rf /var/lib/apt/lists/*
+RUN apt-get -y update && apt-get -y upgrade && apt-get install -y sane sane-utils libusb-0.1 ghostscript netpbm ocrmypdf && apt-get -y clean && rm -rf /var/lib/apt/lists/*
 
 COPY drivers /opt/brother/docker_skey/drivers
 RUN dpkg -i /opt/brother/docker_skey/drivers/*.deb
--- scripts/scan2pdfc.sh
+++ scripts/scan2pdfc.sh
@@ -9,23 +9,12 @@ resolution=300  # 100|150|200|300|400|600|1200|2400|4800|9600
 papersize="letter"   # letter, legal, a4
 mode="24bit Color[Fast]"  # Black & White|Gray[Error Diffusion]|True Gray|24bit Color|24bit Color[Fast]
 
-if [[ $INTR == "true" ]]; then
-    logging="> /var/log/scanner.log 2>&1"
+if [[ $1 == "" ]]; then
+    device="brother4:net1;dev0"
 else
-    logging=""
+    device=$1
 fi
-
-# if [[ $1 == "" ]]; then
-#     device="brother4:net1;dev0"
-# else
-#     device=$1
-# fi
-
-device="brother4:net1;dev0"
-
-[[ $INTR == "true" ]] && echo "Device ID: $device" "$logging"
-
-#[[ $2 == "" ]] && INTR="true"
+[[ $2 == "" ]] && INTR="true"
 
 case $papersize in 
     "letter") w=215.9; h=279.4 ;;
@@ -33,29 +22,29 @@ case $papersize in
     "legal") w=215.9; h=355.6 ;;
 esac
 
-sleep  0.1
+sleep  0.01
 
 filename=/scans/$(date +%F | sed s/-//g)$(date +%T | sed s/://g)
 mkdir -p $filename
 
-[[ $INTR == "true" ]] && echo "Acquiring image(s) ..." "$logging"
+[[ $INTR == "true" ]] && echo "Acquiring image(s) ..."
 
-scanadf --device-name "$device" --resolution "$resolution" --mode "$mode" -x $w -y $h -o "$filename"/image_%04d "$logging"
+scanadf --device-name "$device" --resolution "$resolution" --mode "$mode" -x $w -y $h -o"$filename"/image_%04d
 
-[[ $INTR == "true" ]] && echo "Converting to PDF ..." "$logging"
+[[ $INTR == "true" ]] && echo "Converting to PDF ..."
 
 for pnmfile in $filename/*; do
    pnmtops -dpi=$resolution -equalpixels "$pnmfile"  > "$pnmfile".ps 2> /dev/null
 done
 
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -sOutputFile="$filename"/tmp.pdf $(ls "$filename"/*.ps) "$logging"
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -sOutputFile="$filename"/tmp.pdf $(ls "$filename"/*.ps) 
 
-[[ $INTR == "true" ]] && echo "OCRing PDF ..." "$logging"
+[[ $INTR == "true" ]] && echo "OCRing PDF ..."
 
-ocrmypdf --rotate-pages --deskew "$filename"/tmp.pdf "$filename".pdf "$logging"
+ocrmypdf --rotate-pages --deskew "$filename"/tmp.pdf "$filename".pdf
 
-[[ $INTR == "true" ]] && echo "Cleaning up ..." "$logging"
+[[ $INTR == "true" ]] && echo "Cleaning up ..."
 
-rm -rf $filename "$logging"
+rm -rf $filename
 
-[[ $INTR == "true" ]] && echo "Done." "$logging"
+[[ $INTR == "true" ]] && echo "Done."
