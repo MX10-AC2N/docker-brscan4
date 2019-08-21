15c860c8dd16696baeee3edfa328480d57e5894f
Zak Dehlawi <zdehlawi@gmail.com>
2019-08-21 17:06:33 -0400

    Revert "Migrated to scanimage to deal with scanadf hanging with bed scanning" as it didn't fix the issue
    This reverts commit 62b760ca37b21084030fdb73023a5c8ae4c33337.

--- scripts/scan2pdfc.sh
+++ scripts/scan2pdfc.sh
@@ -35,9 +35,7 @@ mkdir -p $filename
 # workaround to query scanners
 scanadf -L
 
-#scanadf --device-name "$device" --resolution "$resolution" --mode "$mode" -x $w -y $h -o "$filename"/image_%04d
-
-scanimage --device-name "$device" --resolution "$resolution" --mode "$mode" -x $w -y $h --batch="$filename"/image_%04d
+scanadf --device-name "$device" --resolution "$resolution" --mode "$mode" -x $w -y $h -o "$filename"/image_%04d
 
 [[ $INTR == "true" ]] && echo "Converting to PDF ..."
 
