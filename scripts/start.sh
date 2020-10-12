5c370b46cb28f1767cb3c1d5f776b16c4345ea12
Zakariya Dehlawi <1308071+Zaxim@users.noreply.github.com>
2020-10-12 17:09:05 -0400

    switch back to root user for debugging

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
-/usr/sbin/useradd --system scan_user -u $PUID
+#/usr/sbin/useradd --system scan_user -u $PUID
 chown -R $PUID:$PGID /scans
 /usr/bin/brsaneconfig4 -a name=$SCANNER_NAME model=$SCANNER_MODEL ip=$SCANNER_IP_ADDRESS
-su scan_user -c "/usr/bin/brscan-skey -f"
+#su scan_user -c "/usr/bin/brscan-skey -f"
+/usr/bin/brscan-skey -f
